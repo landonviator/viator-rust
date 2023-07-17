e6cf9a077e62b207a9f30aaaf2328399ffa19f34
Landon <33662627+landonviator@users.noreply.github.com>
2023-07-16 21:28:41 -0500

    Initial commit

---
+++ viator-rust/JuceLibraryCode/BinaryData.h
@@ -0,0 +1,114 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   decor_grid_png;
+    const int            decor_grid_pngSize = 39416;
+
+    extern const char*   screw_png;
+    const int            screw_pngSize = 12789;
+
+    extern const char*   Ver_flat_slider_png;
+    const int            Ver_flat_slider_pngSize = 294674;
+
+    extern const char*   Ver_slider_png;
+    const int            Ver_slider_pngSize = 6785719;
+
+    extern const char*   Knob_01_png;
+    const int            Knob_01_pngSize = 1249269;
+
+    extern const char*   Knob_02_png;
+    const int            Knob_02_pngSize = 1789961;
+
+    extern const char*   Knob_03_png;
+    const int            Knob_03_pngSize = 2122248;
+
+    extern const char*   Knob_04_png;
+    const int            Knob_04_pngSize = 1798923;
+
+    extern const char*   Knob_05_png;
+    const int            Knob_05_pngSize = 2138199;
+
+    extern const char*   Knob_06_png;
+    const int            Knob_06_pngSize = 1744482;
+
+    extern const char*   Knob_07_png;
+    const int            Knob_07_pngSize = 1522716;
+
+    extern const char*   Knob_08_png;
+    const int            Knob_08_pngSize = 2017726;
+
+    extern const char*   Knob_09_png;
+    const int            Knob_09_pngSize = 1679558;
+
+    extern const char*   Knob_10_png;
+    const int            Knob_10_pngSize = 1753539;
+
+    extern const char*   back_vumeter_decore5_png;
+    const int            back_vumeter_decore5_pngSize = 102641;
+
+    extern const char*   scale_vumeter_png;
+    const int            scale_vumeter_pngSize = 9539;
+
+    extern const char*   scale_vumeterno_name_png;
+    const int            scale_vumeterno_name_pngSize = 8275;
+
+    extern const char*   vu_meter_png;
+    const int            vu_meter_pngSize = 5705215;
+
+    extern const char*   back_a_png;
+    const int            back_a_pngSize = 3176564;
+
+    extern const char*   back_d_png;
+    const int            back_d_pngSize = 2783376;
+
+    extern const char*   but_small_rec_off_png;
+    const int            but_small_rec_off_pngSize = 13091;
+
+    extern const char*   but_small_rec_on_png;
+    const int            but_small_rec_on_pngSize = 11697;
+
+    extern const char*   landon_png;
+    const int            landon_pngSize = 26270;
+
+    extern const char*   LED_meter_png;
+    const int            LED_meter_pngSize = 157166;
+
+    extern const char*   led_off_png;
+    const int            led_off_pngSize = 5637;
+
+    extern const char*   led_on_png;
+    const int            led_on_pngSize = 6026;
+
+    extern const char*   pad_off_png;
+    const int            pad_off_pngSize = 44456;
+
+    extern const char*   pad_on_png;
+    const int            pad_on_pngSize = 48765;
+
+    extern const char*   screen_png;
+    const int            screen_pngSize = 69252;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 29;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
